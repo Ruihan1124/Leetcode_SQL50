d38c2a94d999651247f2af8431824a8f921202eb
Ruihan <ruihan.liu@u.nus.edu>
2025-05-29 19:29:24 +0800

    Update 595. Big Countries.sql

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -1,8 +1,9 @@
 -- 595. Big Countries
 -- 2nd practice: 2025/5/26
+-- SELECT
 
 SELECT name, population, area
 FROM World
-WHERE area >= '3000000' or population >= '25000000'
+WHERE area >= '3000000' OR population >= '25000000'
 
 -- notes: very easy
