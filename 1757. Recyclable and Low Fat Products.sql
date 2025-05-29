6865cb832b2e3872859f20d70a5c395fc37da328
Ruihan <ruihan.liu@u.nus.edu>
2025-05-29 19:22:20 +0800

    Update 1757. Recyclable and Low Fat Products.sql

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -1,9 +1,10 @@
 -- 1757. Recyclable and Low Fat Products
--- second time practice 2025/5/26
+-- SELECT
+-- 2nd practice 2025/5/26
 
 SELECT product_id 
 FROM Products
 WHERE low_fats = 'Y' and recyclable = 'Y'
 
--- notes: easy level
+-- notes: very very easy
 -- understand of fundemental function of 'SELECT'
