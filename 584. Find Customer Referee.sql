2df824d3a29120c90b62b51bdf7949d8c83eab36
Ruihan <ruihan.liu@u.nus.edu>
2025-05-29 19:28:54 +0800

    Update 584. Find Customer Referee.sql

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,9 +1,21 @@
 -- 584. Find Customer Referee
--- second time practice 2025/5/26
+-- 2nd practice 2025/5/26
+-- SELECT
+
+-- Method 1:
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+OR referee_id is null
+
+-- Method 2:
 
 SELECT name
 FROM Customer
-WHERE referee_id != '2'
+WHERE referee_id <> '2'
 OR referee_id is null
 
--- notes: correct classification
+-- notes: 
+-- 1. correct classification: not referered by 2, might be null.
+-- 2. two kinds of 'not equal to' in SQL.  <> more common. != only in MySQL and PostgreSQL, not in Oracle.
+-- 3. since referer_id are int, both 2 and '2' is ok. If referer_id is varchar, only '2' is ok.
